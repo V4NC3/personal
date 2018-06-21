a3c94de9c483f79441c403dd9442e7c4998b3995
Gilbert Wong <gilwong3105@gmail.com>
2018-06-21 02:07:45 -0700

    setting up initial UI page

--- client/src/components/UI/Footer.vue
+++ client/src/components/UI/Footer.vue
@@ -1,13 +1,22 @@
 <template>
-    <div>Footer</div>
+    <div class='app-footer'></div>
 </template>
 
 <script>
 export default {
-
+    name: 'Footer'
 }
 </script>
 
-<style>
-
+<style scoped>
+    .app-footer {
+        position: fixed;
+        left: 0;
+        bottom: 0;
+        width: 100%;
+        color: #fff;
+        background-color: blue;
+        text-align: center;
+        height: 56px;
+    }
 </style>
--- client/src/components/UI/Navbar.vue
+++ client/src/components/UI/Navbar.vue
@@ -1,13 +1,36 @@
 <template>
-    <div>Nav bar</div>
+    <nav class="navbar navbar-expand-lg app-navbar">
+        <a class="navbar-brand" href="#">App</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav ml-auto">
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Login</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Register</a>
+                </li>
+                
+            </ul>
+        </div>
+    </nav>
 </template>
 
 <script>
-export default {
-
-}
+    export default {
+        name: "Navbar"
+    };
 </script>
 
-<style>
+<style scoped>
+    .app-navbar {
+        background-color: blue;
+        width: 100%;
+    }
 
+    a {
+        color: #fff !important;
+    }
 </style>
