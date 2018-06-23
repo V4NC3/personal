e8e7b7a1ded0affd007bbc510a523bae0d730434
Gilbert Wong <gilwong3105@gmail.com>
2018-06-23 14:10:21 -0700

    more changes to friend table sql file

--- server/SQL/FriendTable.sql
+++ server/SQL/FriendTable.sql
@@ -5,9 +5,9 @@
 # http://www.sequelpro.com/
 # https://github.com/sequelpro/sequelpro
 #
-# Host: localhost (MySQL 5.6.35)
+# Host: localhost (MySQL 5.6.38)
 # Database: social-network
-# Generation Time: 2018-06-21 07:11:29 +0000
+# Generation Time: 2018-06-23 21:09:13 +0000
 # ************************************************************
 
 
@@ -26,13 +26,15 @@
 DROP TABLE IF EXISTS `Friends`;
 
 CREATE TABLE `Friends` (
+  `Id` int(11) NOT NULL AUTO_INCREMENT,
   `UserId` int(11) NOT NULL,
-  `FirstName` varchar(250) NOT NULL,
-  `LastName` varchar(250) NOT NULL,
-  `Age` int(11) NOT NULL,
-  `Email` varchar(250) NOT NULL,
+  `FriendId` int(11) NOT NULL,
+  `IsFriend` tinyint(1) DEFAULT NULL,
+  PRIMARY KEY (`Id`),
   KEY `UserId` (`UserId`),
-  CONSTRAINT `friends_ibfk_1` FOREIGN KEY (`UserId`) REFERENCES `Users` (`Id`)
+  KEY `FriendId` (`FriendId`),
+  CONSTRAINT `friends_ibfk_1` FOREIGN KEY (`UserId`) REFERENCES `Users` (`Id`),
+  CONSTRAINT `friends_ibfk_2` FOREIGN KEY (`FriendId`) REFERENCES `Users` (`Id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 
