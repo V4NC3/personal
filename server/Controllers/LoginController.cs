a65b57b6372a82b5a3acddaae751835038ccdc04
VAN NGUYEN <nguyen.vance@gmail.com>
2018-07-18 23:15:16 -0700

    added condition check for correct returning JWT

--- server/Controllers/LoginController.cs
+++ server/Controllers/LoginController.cs
@@ -51,23 +51,30 @@ namespace server.Controllers
         public async IActionResult Token();
         {
             //string tokenString = "test";
-            var claimsdata = new[] {new Claim(ClaimTypes.Name, "USER_NAME")};
+            var header = Request.Headers["Authorization"]
+            if(header.ToString().StartsWith("Basic"))
+            {
+                var credValue = header.ToString().Substring("Basic ".Length).Trim();
+                var clientAndPassenc = Encoding.UTF8.GetString(Convert.FromBase64String(credValue));
+                var claimsdata = new[] {new Claim(ClaimTypes.Name, "USER_EMAIL")};
 
-            //using secret key
-            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("SecretKeyStoredInConfigFile"))
-            var signingCred = new signingCredentials(key, SecurityAlgorithms.HmacSha256Signature);
-            var tokenString = new JwtSecurityToken(
-                issuer:"mysite.com"
-                , audience: "mysite.com"
-                , expires: DateTime.Now.AddMinutes(1)
-                , claims: claimsdata
-                // using secret key
-                , signingCredentials: signingCred
-            );
-            var tokenString = new JwtSecurityTokenHandler().WriteToken(token);
-            return Ok(tokenString);
-            
-            //return View();
+                //using secret key
+                var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("SecretKeyStoredInConfigFile"))
+                var signingCred = new signingCredentials(key, SecurityAlgorithms.HmacSha256Signature);
+                var tokenString = new JwtSecurityToken(
+                    issuer:"mysite.com"
+                    , audience: "mysite.com"
+                    , expires: DateTime.Now.AddMinutes(1)
+                    , claims: claimsdata
+                    // using secret key
+                    , signingCredentials: signingCred
+                );
+                var tokenString = new JwtSecurityTokenHandler().WriteToken(token);
+                return Ok(tokenString);
+            }
+            return BadRequest("Wrong Requested Token");
+            //return View();  
+        
         }
     }
 }
